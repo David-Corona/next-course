7029a68ef4cdb24578aaf42597d09fab8cead319
David Corona <corona_121@hotmail.com>
2025-04-25 23:56:14 +0200

    01 complete - routing

---
+++ 01-starting-project/app/about/page.js
@@ -0,0 +1,5 @@
+export default function aboutPage() {
+    return <main>
+        <h1>About Page</h1>
+    </main>
+}
--- 01-starting-project/app/layout.js
+++ 01-starting-project/app/layout.js
@@ -1,5 +1,6 @@
 import './globals.css'
 
+// metadata is a reserved name, it's an object that contains what is usually in the html headers, applied to this layout
 export const metadata = {
   title: 'NextJS Course App',
   description: 'Your first NextJS app!',
